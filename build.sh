b69bc959894b30b79f5d62036de47c3cdd9bd641
pankcuf <rastacake@gmail.com>
2021-12-14 11:46:02 +0300

    [update lib names]

--- build.sh
+++ build.sh
@@ -16,10 +16,11 @@ fi
 cargo +nightly lipo --release
 cargo +nightly build --target=x86_64-apple-darwin --release
 cargo +nightly build --target=aarch64-apple-darwin --release
-lipo -create target/aarch64-apple-darwin/release/libmndiff.a target/x86_64-apple-darwin/release/libmndiff.a -output target/universal/release/libmndiff_macos.a
+lipo -create target/aarch64-apple-darwin/release/libdash_mndiff.a target/x86_64-apple-darwin/release/libdash_mndiff.a -output target/universal/release/libdash_mndiff_macos.a
 
 # Assume we have structure like this:
 # dash/masternodes-diff-processor/...
 # dash/DashSync/...
-cp -Rp target/universal/release/*.a ../DashSync/DashSync/lib/
+cp -p target/universal/release/libdash_mndiff.a ../DashSync/DashSync/lib/libdash_mndiff_ios.a
+cp -p target/universal/release/libdash_mndiff_macos.a ../DashSync/DashSync/lib/libdash_mndiff_macos.a
 cp -p target/dash_mndiff.h ../DashSync/DashSync/shared/crypto/
