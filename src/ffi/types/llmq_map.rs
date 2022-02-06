1e19f26aeeedcf22ee8f87ada4757ea3268aa27a
pankcuf <rastacake@gmail.com>
2022-02-06 08:35:58 +0300

    [remove unused llmq helpers]

--- src/ffi/types/llmq_map.rs
+++
@@ -1,8 +0,0 @@
-use crate::ffi::types::llmq_entry::LLMQEntry;
-
-#[repr(C)] #[derive(Clone, Copy, Debug)]
-pub struct LLMQMap {
-    pub llmq_type: u8,
-    pub values: *mut *mut LLMQEntry,
-    pub count: usize,
-}
