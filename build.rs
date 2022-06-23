de87cd37641b970a18777ba813622f0052976112
pankcuf <rastacake@gmail.com>
2022-06-23 15:44:07 +0300

    [specify cbindgen]

--- build.rs
+++ build.rs
@@ -4,13 +4,20 @@ use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let mut config: cbindgen::Config = Default::default();
+    let mut config: cbindgen::Config = cbindgen::Config::from_file("./cbindgen.toml").expect("Error config");
+    let mut parse_config: cbindgen::ParseConfig = cbindgen::ParseConfig::default();
+    parse_config.parse_deps = true;
+    // Here we write down crate names (!) where we want to retrieve C-bindings
+    let includes = vec![
+        "dash-spv-ffi".to_string(),
+        "dash-spv-models".to_string()
+    ];
+    parse_config.include = Some(includes.clone());
+    parse_config.extra_bindings = includes.clone();
     config.language = cbindgen::Language::C;
-    match cbindgen::generate_with_config(&crate_dir, config) {
-        Result::Ok(bindings) => bindings.write_to_file("target/dash_mndiff.h"),
-        Result::Err(err) => {
-            eprintln!("Error generating: {}", err);
-            false
-        }
-    };
+    config.parse = parse_config;
+    cbindgen::generate_with_config(&crate_dir, config)
+        .unwrap()
+        .write_to_file("target/dash_mndiff.h");
+
 }
