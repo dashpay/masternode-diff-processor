9d07fee3201f66f10cd9d316ca8470ca1d04dc5f
pankcuf <rastacake@gmail.com>
2022-09-01 19:24:31 +0300

    [more precise processing error]

--- src/processing/processing_error.rs
+++ src/processing/processing_error.rs
@@ -3,18 +3,20 @@
 #[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Hash, Ord)]
 pub enum ProcessingError {
     None = 0,
-    Skipped = 1,
-    ParseError = 2,
-    HasNoBaseBlockHash = 3,
+    PersistInRetrieval = 1,
+    LocallyStored = 2,
+    ParseError = 3,
+    HasNoBaseBlockHash = 4,
 }
 
 impl From<u8> for ProcessingError {
     fn from(orig: u8) -> Self {
         match orig {
             0 => ProcessingError::None,
-            1 => ProcessingError::Skipped,
-            2 => ProcessingError::ParseError,
-            3 => ProcessingError::HasNoBaseBlockHash,
+            1 => ProcessingError::PersistInRetrieval,
+            2 => ProcessingError::LocallyStored,
+            3 => ProcessingError::ParseError,
+            4 => ProcessingError::HasNoBaseBlockHash,
             _ => ProcessingError::None,
         }
     }
@@ -24,9 +26,10 @@ impl Into<u8> for ProcessingError {
     fn into(self) -> u8 {
         match self {
             ProcessingError::None => 0,
-            ProcessingError::Skipped => 1,
-            ProcessingError::ParseError => 2,
-            ProcessingError::HasNoBaseBlockHash => 3,
+            ProcessingError::PersistInRetrieval => 1,
+            ProcessingError::LocallyStored => 2,
+            ProcessingError::ParseError => 3,
+            ProcessingError::HasNoBaseBlockHash => 4,
         }
     }
 }
