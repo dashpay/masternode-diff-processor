5dbc421102056842066c42a504a49ffaf6de05e7
pankcuf <rastacake@gmail.com>
2023-04-16 17:48:10 +0400

    [refac]

--- src/util/data_ops.rs
+++ src/util/data_ops.rs
@@ -1,4 +1,5 @@
-use hashes::Hash;
+use secp256k1::rand;
+use secp256k1::rand::Rng;
 use std::fmt::Write;
 use crate::consensus::Encodable;
 use crate::crypto::UInt256;
@@ -7,12 +8,8 @@ pub const DASH_MESSAGE_MAGIC: &str = "DarkCoin Signed Message:\n";
 
 #[inline]
 pub fn random_initialization_vector_of_size(size: usize) -> Vec<u8> {
-    use secp256k1::rand;
-    use secp256k1::rand::distributions::Uniform;
-    use secp256k1::rand::Rng;
     let mut rng = rand::thread_rng();
-    let range = Uniform::new(0, 255);
-    (0..size).map(|_| rng.sample(&range)).collect()
+    (0..size).map(|_| rng.gen_range(0..=255)).collect()
 }
 
 
@@ -39,21 +36,23 @@ pub fn short_hex_string_from(data: &[u8]) -> String {
 #[inline]
 pub fn merkle_root_from_hashes(hashes: Vec<UInt256>) -> Option<UInt256> {
     let length = hashes.len();
-    let mut level = hashes.clone();
-    if length == 0 { return None; }
-    if length == 1 { return Some(hashes[0]); }
-    while level.len() != 1 {
-        let len = level.len();
-        let capacity = (0.5 * len as f64).round();
-        let mut higher_level: Vec<UInt256> = Vec::with_capacity(capacity as usize);
-        for i in (0..len).step_by(2) {
-            let mut buffer: Vec<u8> = Vec::with_capacity(64);
-            let left = level[i];
-            left.enc(&mut buffer);
-            if len > i + 1 { level[i+1] } else { left }.enc(&mut buffer);
-            higher_level.push(UInt256(hashes::sha256d::Hash::hash(&buffer).into_inner()));
+    let mut level = hashes;
+    match length {
+        0 => None,
+        1 => Some(level[0]),
+        _ => {
+            while level.len() != 1 {
+                let len = level.len();
+                let mut higher_level = Vec::<UInt256>::with_capacity((0.5 * len as f64).ceil() as usize);
+                for pair in level.chunks(2) {
+                    let mut buffer = Vec::with_capacity(64);
+                    pair[0].enc(&mut buffer);
+                    (pair.get(1).unwrap_or(&pair[0])).enc(&mut buffer);
+                    higher_level.push(UInt256::sha256d(buffer));
+                }
+                level = higher_level;
+            }
+            Some(level[0])
         }
-        level = higher_level;
     }
-    Some(level[0])
 }
