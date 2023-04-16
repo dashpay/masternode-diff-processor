c4d2dbbc21ad078a33f4dc0bbc337dfe9b01f898
pankcuf <rastacake@gmail.com>
2023-04-16 17:50:33 +0400

    [impl byteread socketaddress]

--- src/common/socket_address.rs
+++ src/common/socket_address.rs
@@ -1,4 +1,5 @@
 use std::io;
+use byte::{BytesExt, TryRead};
 use crate::consensus::Encodable;
 use crate::crypto::UInt128;
 
@@ -15,5 +16,14 @@ impl Encodable for SocketAddress {
         self.ip_address.enc(&mut s);
         self.port.swap_bytes().enc(&mut s);
         Ok(18)
+    }
+}
+
+impl<'a> TryRead<'a, ()> for SocketAddress {
+    fn try_read(bytes: &'a [u8], context: ()) -> byte::Result<(Self, usize)> {
+        let offset = &mut 0;
+        let ip_address = bytes.read_with::<UInt128>(offset, byte::LE)?;
+        let port = bytes.read_with::<u16>(offset, byte::BE)?;
+        Ok((SocketAddress { ip_address, port }, *offset))
     }
 }
